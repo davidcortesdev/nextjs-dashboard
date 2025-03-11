c8bc5edfe6ff5891116eb516b69bdc63284e90a1
davidcortesdev <davidcortes.dev@gmail.com>
2025-03-11 12:41:13 +0100

    nextjs-dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import { montserrat } from './ui/fonts';
+import './ui/global.css'
+
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+        <footer className='py-10 flex justify-center items-center'>
+          Este es el footer de prueba
+        </footer>
+      </body>
     </html>
   );
 }
